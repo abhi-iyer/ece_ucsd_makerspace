969154a5a414b1472daf5f1cb180484fbba889cf
Abhiram Iyer <abhi.iyer.ai@gmail.com>
2017-07-28 16:13:33 -0700

    Basic queryDB() function to see if sqlite3 db has entry with given student PID

--- loginsys/views.py
+++ loginsys/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.db import connection
 
 def index(request):
 	context = {'title': 'Main Login'}
@@ -8,3 +9,11 @@ def index(request):
 
 def thanks(request):
 	return HttpResponse('Thank You!')
+
+def queryDB(pid=Student.objects.get(pid)):
+	with connection.cursor() as cursor:
+		cursor.execute("SELECT * FROM students WHERE pid = '%s'" %pid)
+		row = cursor.fetchone()
+		while row is not None:
+			print(row)	# typically only one row should be printed, as only one person has this unique PID
+			row = cursor.fetchone()
