4754e9b819c6724b22f399f69634d6818521e256
Abhiram Iyer <abhi.iyer.ai@gmail.com>
2017-08-01 21:30:23 -0700

    Fixed bad indentation

--- loginsys/parser.py
+++ loginsys/parser.py
@@ -1,5 +1,5 @@
 def card_parse(input):
-    if (input[0] == ';'): 
-    	student_pid = input[2:11] # magnetic-strip card swiper's rules
-        return student_pid
+    if (input[0] == ';'):
+	student_pid = input[2:11] # magnetic-strip card swiper's rules
+	return student_pid
     # else parse with barcode parser's rules 
