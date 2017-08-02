7cb6d0dbc8e05ee13c3ac83fda030d4e8b116f1b
Abhi Iyer <abhi.iyer.ai@gmail.com>
2017-08-02 15:46:20 -0700

    student listings in admin page now display last_name, first_name

--- loginsys/models.py
+++ loginsys/models.py
@@ -3,22 +3,25 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Student(models.Model):
-	last_name = models.CharField(max_length=20, null=True)
-	first_name = models.CharField(max_length=20, null=True)
-	pid = models.CharField(max_length=9, null=True)
-	employee_num = models.CharField(max_length=20, null=True)
-	class_org = models.CharField(max_length=20, null=True)
-	dept = models.CharField(max_length=20, null=True)
-	class_year = models.CharField(max_length=15, null=True)
-	suspended = models.BooleanField(default=False)
+        last_name = models.CharField(max_length=20, null=True)
+        first_name = models.CharField(max_length=20, null=True)
+        pid = models.CharField(max_length=9, null=True)
+        employee_num = models.CharField(max_length=20, null=True)
+        class_org = models.CharField(max_length=20, null=True)
+        dept = models.CharField(max_length=20, null=True)
+        class_year = models.CharField(max_length=15, null=True)
+        suspended = models.BooleanField(default=False)
+
+        def __str__(self):
+            return '%s, %s' % (self.last_name, self.first_name)
 
 class AdminInfo(models.Model):
-	student = models.ForeignKey(Student)
-	printer_train = models.DateTimeField('3D-Printer')
-	laser_train = models.DateTimeField('Laser Cutter')
+        student = models.ForeignKey(Student)
+        printer_train = models.DateTimeField('3D-Printer')
+        laser_train = models.DateTimeField('Laser Cutter')
 
 class Log(models.Model):
-	student = models.ForeignKey(Student)
-	login = models.DateTimeField('Log Into Lab')
-	logout = models.DateTimeField('Log Out of Lab')
+        student = models.ForeignKey(Student)
+        login = models.DateTimeField('Log Into Lab')
+        logout = models.DateTimeField('Log Out of Lab')
 
