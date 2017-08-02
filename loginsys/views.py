75e6277792249f15e2f9bda54ae473b95dd7e736
Abhi Iyer <abhi.iyer.ai@gmail.com>
2017-08-02 15:07:24 -0700

    Added error checking to views.py and helper_functions.py

--- loginsys/views.py
+++ loginsys/views.py
@@ -2,11 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from .helper_functions import *
+
 def index(request):
 	context = {'title': 'Main Login'}
 	return render(request, 'loginsys/index.html', context)
 
 def student_info(request):
     if (request.method == "POST"):
-        student = get_student(card_parse(request.POST['pid']))
-        return HttpResponse(student)
+        pid = card_parse(request.POST['pid'])
+        if (pid != 0):
+            student = get_student(pid)
+            return HttpResponse(student)
+        else:
+            return HttpResponse("Invalid card. Please use an official Student ID card issued by UC San Diego.")
+    else:
+        return HttpResponse("Please enter your PID in the appropriate field, not in the URL.")
