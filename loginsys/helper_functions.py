b75df75197de8e2b7b0385dfae181e617398e25d
Abhiram Iyer <abhi.iyer.ai@gmail.com>
2017-08-05 15:28:22 -0700

    fixed invalid-card-or-not logic

--- loginsys/helper_functions.py
+++ loginsys/helper_functions.py
@@ -1,17 +1,17 @@
 from .models import Student
 def get_student(pid):
-    try:
-        student = Student.objects.get(pid=pid)
-        return student.first_name + ' ' + student.last_name
-    except:
-        return "NE" #No entry
+	try:
+		student = Student.objects.get(pid=pid)
+		return student.first_name + ' ' + student.last_name
+	except:
+		return "NE" #No entry
 
 def card_parse(input):
-    if input :
-        if ( input[0] == ';' ):
-          # magnetic-strip card swiper's rules
-          student_pid = input[2:11]
-          return student_pid
-    else:
-        return 0
+	if input:
+		if ( input[0] == ';' ):
+			# magnetic-strip card swiper's rules
+			student_pid = input[2:11]
+			return student_pid
+		else:
+			return 0
 
