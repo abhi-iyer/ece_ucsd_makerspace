670d974d8c2c74c8491120cc3a607e7e5a2b0021
Abhiram Iyer <abhi.iyer.ai@gmail.com>
2017-08-07 14:59:51 -0700

    views.py now handles every situation where student swipes card and stores every login into a persistent adminlog item in db

--- loginsys/views.py
+++ loginsys/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-#from django.views.decorators.csrf import csrf_exempt
 from .helper_functions import *
 import json
 import time
+from datetime import datetime
+from .models import *
+from django.utils import timezone
 
 def index(request):
     context = {'title': 'Main Login'}
@@ -16,18 +18,36 @@ def student_info(request):
         time.sleep(3)
         if (pid != 0):
             student = get_student(pid)
-            if student != 'NE' :
-              data = {'status':'OK', 'data':student}
+            if student != None: # student found in database
+              
+              if (student.suspended == False): # student is not suspended
+                data = {'status':'OK', 'data':student.first_name + " " + student.last_name}
+                
+                log = AdminLog(student=student, date=timezone.now(), success=AdminLog.SUCCESS)
+                log.save()     
+  
+                return HttpResponse(json.dumps(data))
+              else:  # student is suspended
+                data = {'status':'NOK', 'data':student.first_name + " " + student.last_name + " is suspended"}
+                
+                log = AdminLog(student=student, date=timezone.now(), success=AdminLog.SUSPENDED)
+                log.save()
+                
+                return HttpRequest(json.dumps(data))                
 
-              num_people = request.session.get('num_people', 0)
-              request.session['num_people'] = num_people+1
-
-              return HttpResponse(json.dumps(data))
-            else:
+            else: # student not found in data base
               data = {'status':'NE', 'data':''}
+              
+              log = AdminLog(student=None, date=timezone.now(), success=AdminLog.FAILURE)
+              log.save()
+            
               return HttpResponse(json.dumps(data))
         else:
             data = {'status':'ERROR', 'data': 'Invalid card. Please use an official Student ID card issued by UC San Diego.'}
+            
+            log = AdminLog(student=None, date=timezone.now(), success=AdminLog.INVALID)
+            log.save()
+            
             return HttpResponse(json.dumps(data))
     else:
         return HttpResponse('Please enter your PID in the appropriate field, not in the URL.')
