7c4b7df516bb1cc358db12137fadb070d31964ea
Abhiram Iyer <abhi.iyer.ai@gmail.com>
2017-08-07 15:01:25 -0700

    --file not used temporarily-- contains updated cron job code for sessions framework

--- loginsys/MyCron.py
+++ loginsys/MyCron.py
@@ -1,18 +1,24 @@
-from django_cron import CronJobBase, Schedule
-from django.http import HttpResponse
-from .models import Log
+fom django_cron import CronJobBase, Schedule
+from django.contrib.sessions.backends.db import SessionStore
+from django.contrib.sessions.models import Session
+from .models import AdminLog
 from datetime import datetime
 
-def CronJob(CronJobBase):
-    RUN_AT_TIMES = [ '23:00' ]
+class MyCronJob(CronJobBase):
+    # RUN_AT_TIMES = [ '23:00' ]
 
-    schedule = Schedule(run_at_times=RUN_AT_TIMES)
+    # schedule = Schedule(run_at_times=RUN_AT_TIMES)
+    
+    RUN_EVERY_MINS = 1
+    schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
+
     code = 'loginsys.new_session'  # a unique code
 
     def do(self):
-        num_people = request.session.get('num_people')
-	log = Log(datetime.now(), num_people)
-	log.save()
-	
-	del request.session['num_people']
+        session = SessionStore()
+        num_people = Session.objects.get('num_people')
+        log = Log(datetime.now(), num_people)
+        log.save()
+        
+        session["num_people"] = 0
 
