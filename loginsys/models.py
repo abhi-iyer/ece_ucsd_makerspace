6a7f8ae431e45baf7f952584e807598f040cdb56
Abhiram Iyer <abhi.iyer.ai@gmail.com>
2017-08-07 14:55:58 -0700

    updated AdminLog model to have new fields to keep track of which student has logged in

--- loginsys/models.py
+++ loginsys/models.py
@@ -7,7 +7,6 @@ class Student(models.Model):
         first_name = models.CharField(max_length=20, null=True)
         pid = models.CharField(max_length=9, null=True)
         employee_num = models.CharField(max_length=20, null=True)
-        class_org = models.CharField(max_length=20, null=True)
         dept = models.CharField(max_length=20, null=True)
         class_year = models.CharField(max_length=15, null=True)
         suspended = models.BooleanField(default=False)
@@ -20,14 +19,32 @@ class AdminInfo(models.Model):
         printer_train = models.DateTimeField('3D-Printer')
         laser_train = models.DateTimeField('Laser Cutter')
 
-class Log(models.Model):
-        # student = models.ForeignKey(Student)
-        date = models.DateTimeField('Date')
-        users = models.IntegerField('Num of users')
+class AdminLog(models.Model):
+        student = models.ForeignKey(Student)
+        date = models.DateTimeField()
+        
+        SUCCESS='SUCC'
+        FAILURE='FAIL'
+        SUSPENDED="SUSP"
+        INVALID="INVA"
+        
+        
+        SUCCESS_CHOICES = (
+            (SUCCESS, "Success"),
+            (FAILURE, "Student Not Found"),
+            (SUSPENDED, "Access Revoked"),
+            (INVALID, "Invalid Card"),
+        )        
 
-        def create(cls, d, u):
-            log = cls(date = d, users=u)    
-            return log
-        # login = models.DateTimeField('Log Into Lab')
-        # logout = models.DateTimeField('Log Out of Lab')
+        success = models.CharField(
+            max_length=4,
+            choices=SUCCESS_CHOICES,
+            default=SUCCESS,        
+        )
+        
+        def __str__(self):
+            return "%s, %s" % (self.student.last_name, self.student.first_name)
 
+       # def create(cls, student, date, success):
+        #    log = cls(student_id=student, date=date, success=success)    
+         #   return log
