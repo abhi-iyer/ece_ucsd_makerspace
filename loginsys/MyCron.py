7ea4a049b3228df64fbf1e941804af600f753167
Abhiram Iyer <abhi.iyer.ai@gmail.com>
2017-08-06 19:21:43 -0700

    basic cron job to store total number of people who have entered the lab every day at 11pm. stores info as a Log object in database - logs can be queried by date

--- loginsys/MyCron.py
+++ loginsys/MyCron.py
@@ -1,13 +1,18 @@
 from django_cron import CronJobBase, Schedule
 from django.http import HttpResponse
+from .models import Log
+from datetime import datetime
 
 def CronJob(CronJobBase):
     RUN_AT_TIMES = [ '23:00' ]
 
     schedule = Schedule(run_at_times=RUN_AT_TIMES)
-    code = 'loginsys.reset_system'  # a unique code
+    code = 'loginsys.new_session'  # a unique code
 
     def do(self):
         num_people = request.session.get('num_people')
-
+	log = Log(datetime.now(), num_people)
+	log.save()
+	
+	del request.session['num_people']
 
