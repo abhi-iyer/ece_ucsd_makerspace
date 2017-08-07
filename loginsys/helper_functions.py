63faba644fba28ece20052e8bf62a380be0b0e44
Abhiram Iyer <abhi.iyer.ai@gmail.com>
2017-08-07 14:55:05 -0700

    updated get_student() method

--- loginsys/helper_functions.py
+++ loginsys/helper_functions.py
@@ -2,9 +2,9 @@ from .models import Student
 def get_student(pid):
 	try:
 		student = Student.objects.get(pid=pid)
-		return student.first_name + ' ' + student.last_name
+		return student
 	except:
-		return "NE" #No entry
+		return None #No entry
 
 def card_parse(input):
 	if input:
