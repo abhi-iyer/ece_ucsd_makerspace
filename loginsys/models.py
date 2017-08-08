45d4b6b45013d2015f7db8a8b7c0244aeec0784e
Abhiram Iyer <abhi.iyer.ai@gmail.com>
2017-08-08 15:52:48 -0700

    updated how adminlog model db items show up in admin page

--- loginsys/models.py
+++ loginsys/models.py
@@ -9,10 +9,9 @@ class User(models.Model):
         employee_num = models.CharField(max_length=20, null=True)
         dept = models.CharField(max_length=20, null=True)
         class_year = models.CharField(max_length=15, null=True)
+        administrator = models.BooleanField(default=False)     
         suspended = models.BooleanField(default=False)
         
-        administrator = models.BooleanField(default=False)
-             
         def __str__(self):
             return '%s %s' % (self.first_name, self.last_name)
 
@@ -21,9 +20,8 @@ class AdminLog(models.Model):
         first_name = models.CharField(max_length=20, null=True)
         pid = models.CharField(max_length=9, null=True)
         error = models.BooleanField(default=False)        
-        
         administrator = models.BooleanField(default=False)
-        
+        suspended = models.BooleanField(default=False)
         date = models.DateTimeField()
         
         SUCCESS="SUCC"
@@ -46,17 +44,17 @@ class AdminLog(models.Model):
         )
         
         def __str__(self):
-            if (self.error != True):
+            if (self.error == False and self.suspended == False):
                 if (self.administrator == True):
-                    return "TA %s %s logged in successfully" % (self.first_name, self.last_name)
+                    return 'TA %s %s logged in successfully' % (self.first_name, self.last_name)
                 else:
-                    return "Student %s %s logged in successfully" % (self.first_name, self.last_name)
+                    return 'Student %s %s logged in successfully' % (self.first_name, self.last_name)
+            elif (self.suspended == True):
+                if (self.administrator == True):
+                    return 'Access revoked for TA %s %s' % (self.first_name, self.last_name)
+                else:
+                    return 'Access revoked for Student %s %s' % (self.first_name, self.last_name)
             elif (self.login_status == "FAIL"):
-                return "User not found in database"
-            elif (self.login_status == "SUSP"):
-                if (self.administrator == True):
-                    return "Access revoked for TA %s %s" % (self.first_name, self.last_name)
-                else:
-                    return "Access revoked for Student %s %s" % (self.first_name, self.last_name)
+                return 'User not found in database'
             elif (self.login_status == "INVA"):
-                return "Invalid ID card"
+                return 'Invalid ID card'
