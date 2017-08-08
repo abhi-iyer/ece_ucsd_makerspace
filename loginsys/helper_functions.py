ffec92a84c1f607d1e352b367387aedd6302b536
Abhiram Iyer <abhi.iyer.ai@gmail.com>
2017-08-08 15:52:15 -0700

    regular PID input now accepted

--- loginsys/helper_functions.py
+++ loginsys/helper_functions.py
@@ -1,17 +1,36 @@
-from .models import User
+from .models import *
+from datetime import datetime
+from django.utils import timezone
+import string
+
 def get_user(pid):
-	try:
-		user = User.objects.get(pid=pid)
-		return user
-	except:
-		return None #No entry
+    try:
+        user = User.objects.get(pid=pid)
+        return user
+    except:
+        return None #No entry
 
 def card_parse(input):
-	if input:
-		if ( input[0] == ';' ):
-			# magnetic-strip card swiper's rules
-			user_pid = input[2:11]
-			return user_pid
-		else:
-			return 0
+    if input:
+        if ( input[0] == ';' ):
+            # magnetic-strip card swiper's rules
+            user_pid = input[2:11]
+            return user_pid
+        elif (input[0] == 'A' or input[0] == 'a'):
+            l = list(input)
+            l[0] = '9'
+            user_pid = "".join(l)             
+            return user_pid
+        else:
+            return 0
+    else:
+        return 0
+
+def get_logs(date):
+    try:
+        log = AdminLog.objects.get(date=date)
+        return log
+    except:
+        return None # no logs for specified day
+        
 
