5efebaa0261e979feb7e20083efb0b0d5ea28ab3
Abhiram Iyer <abhi.iyer.ai@gmail.com>
2017-08-08 17:36:39 -0700

    created more robust schema: adminLog now contains reference to User db table

--- loginsys/models.py
+++ loginsys/models.py
@@ -5,20 +5,18 @@ from datetime import datetime
 class User(models.Model):
         last_name = models.CharField(max_length=20, null=True)
         first_name = models.CharField(max_length=20, null=True)
-        pid = models.CharField(max_length=9, null=True)
-        employee_num = models.CharField(max_length=20, null=True)
-        dept = models.CharField(max_length=20, null=True)
+        card_id = models.CharField(max_length=9, null=True)
+        current_department = models.CharField(max_length=20, null=True)
         class_year = models.CharField(max_length=15, null=True)
-        administrator = models.BooleanField(default=False)     
-        suspended = models.BooleanField(default=False)
+        currently_administrator = models.BooleanField(default=False)     
+        currently_suspended = models.BooleanField(default=False)
         
         def __str__(self):
             return '%s %s' % (self.first_name, self.last_name)
 
 class AdminLog(models.Model):
-        last_name = models.CharField(max_length=20, null=True)
-        first_name = models.CharField(max_length=20, null=True)
-        pid = models.CharField(max_length=9, null=True)
+        user = models.ForeignKey(User, default=None, null=True)
+        
         error = models.BooleanField(default=False)        
         administrator = models.BooleanField(default=False)
         suspended = models.BooleanField(default=False)
@@ -46,15 +44,15 @@ class AdminLog(models.Model):
         def __str__(self):
             if (self.error == False and self.suspended == False):
                 if (self.administrator == True):
-                    return 'TA %s %s logged in successfully' % (self.first_name, self.last_name)
+                    return 'TA %s %s logged in successfully' % (self.user.first_name, self.user.last_name)
                 else:
-                    return 'Student %s %s logged in successfully' % (self.first_name, self.last_name)
+                    return 'Student %s %s logged in successfully' % (self.user.first_name, self.user.last_name)
             elif (self.suspended == True):
                 if (self.administrator == True):
-                    return 'Access revoked for TA %s %s' % (self.first_name, self.last_name)
+                    return 'Access revoked for TA %s %s' % (self.user.first_name, self.user.last_name)
                 else:
-                    return 'Access revoked for Student %s %s' % (self.first_name, self.last_name)
-            elif (self.login_status == "FAIL"):
+                    return 'Access revoked for Student %s %s' % (self.user.first_name, self.user.last_name)
+            elif (self.error == True and self.login_status == "FAIL"):
                 return 'User not found in database'
-            elif (self.login_status == "INVA"):
+            elif (self.error == True and self.login_status == "INVA"):
                 return 'Invalid ID card'
