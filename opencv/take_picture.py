d91fc976ae7f5b2ceb07ce94b30d31ba97d34383
Abhiram Iyer <abhi.iyer.ai@gmail.com>
2017-08-29 23:14:28 +0000

    minor changes

--- opencv/take_picture.py
+++ opencv/take_picture.py
@@ -1,9 +1,10 @@
 from time import sleep
 from picamera import PiCamera
 
-camera = PiCamera()
-camera.resolution = (1024, 768)
-camera.start_preview()
-
-sleep(2)
-camera.capture('foo.jpeg')
+def take_photo():
+    camera = PiCamera()
+    camera.resolution = (1024, 768)
+    camera.start_preview()
+    sleep(2)
+    camera.capture('foo.jpeg')
+    camera.stop_preview()
