ed4c87e821c3c29c73e388610058e45edc6b7709
Abhiram Iyer <abhi.iyer.ai@gmail.com>
2017-08-31 20:06:01 +0000

    filename change

--- opencv/opencv_video.py
+++ opencv/opencv_video.py
@@ -5,7 +5,7 @@ import picamera
 camera = picamera.PiCamera()
 
 camera.start_preview()
-camera.start_recording('/home/pi/video.h264')
-time.sleep(10)
+camera.start_recording('people_walking.h264')
+time.sleep(15)
 camera.stop_recording()
 camera.stop_preview()
